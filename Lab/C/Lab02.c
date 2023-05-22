62482d2139eb25dd79f55405865cb85ff1535aac
Neoxy <1007050740@qq.com>
2023-05-22 12:50:36 +0800

    代码整理

---
+++ Lab/C/Lab02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+char c1,c2;
+
+c1='a';
+
+c2=c1-500;
+
+printf("%d\n",c2);
+
+int a=-1;
+
+printf("%u\n",a);
+
+float f=-1.0;
+
+printf("%u\n",f);
+
+return 0;
+
+}
