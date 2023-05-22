38eb927129f737b0209235e16d6ce67f87ea45e1
Neoxy <1007050740@qq.com>
2023-05-22 12:59:09 +0800

    cpp: classes

---
+++ CPP/classes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Square {
+	public:
+		double length;
+		double height;
+
+		double get(void);
+};
+
+double Square::get() {
+	return (length * height);
+}
+
+int main() {
+	Square box;
+	box.height = 100.2;
+	box.length = 20.5;
+	double result = box.get();
+	std::cout << result << std::endl;
+}
