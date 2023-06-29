15dae102b8c0212b4961d52e8eed9299c61f606a
Neoxy <1007050740@qq.com>
2023-06-29 08:57:52 +0800

    the last class of C programming language

---
+++ Lab/C/main.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 0
+#include <stdio.h>
+
+int main() {
+
+	FILE *fp=NULL;
+	char buff[255];
+
+	fp = fopen("first.txt", "r");
+	fgets(buff, 255, (FILE*)fp);
+	if ((fp = fopen("C:\\Users\\PCuser\\source\\repos\\Project1\\Project1\\first.txt", "r") )== NULL) {
+		printf("can not open first.txt\n");
+		return 0;
+	}
+
+	if (fp = fopen("C:\\Users\\PCuser\\source\\repos\\Project1\\Project1\\second.txt", "w") == NULL) {
+		printf("can not open second.txt\n");
+		return 0;
+	}
+	fputs(buff, fp);
+
+	return 0;
+}
